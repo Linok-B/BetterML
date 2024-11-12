8058fa9a146459c05ac87f4365cb008d748c605c
Linok-B <123248369+Linok-B@users.noreply.github.com>
2024-11-12 18:32:15 +0100

    Update inject.js

--- src/inject.js
+++ src/inject.js
@@ -3,7 +3,7 @@
 
     const dependencies = ['deobf.js', 'jimp.min.js', 'quantize.js', 'artyom.window.min.js'];
     // loader.js MUST come last and extrasounds.js MUST be first
-    const mods = ['extrasounds.js', 'exporter.js', 'pixelCopyImage.js', 'rankhand.js', 'performance.js', 'friendsplus.js', 'movepainter.js', 'norefresh.js', 'freecam.js', 'importer.js', 'speechtotext.js', 'highcontrast.js', 'debugmode.js', 'buildplus.js', 'farbackconverter.js','loader.js'];
+    const mods = ['extrasounds.js', 'exporter.js', 'pixelCopyImage.js', 'performance.js', 'friendsplus.js', 'movepainter.js', 'freecam.js', 'importer.js', 'speechtotext.js', 'highcontrast.js', 'debugmode.js', 'buildplus.js', 'farbackconverter.js','loader.js'];
 
     // Prime dependencies
     for(d of dependencies) {
